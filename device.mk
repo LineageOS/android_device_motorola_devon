bab6acac523334aa89ec365c1479eda3ba57c55c
Dhina17 <dhinalogu@gmail.com>
2023-02-11 11:21:40 +0530

    hawao: Import audio configs
    - from hawao S2SE32.28-41-1
    
    Change-Id: Ie56322f474648096373e29480e8cb35333d5b7d7

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 # Inherit from sm6225-common
 $(call inherit-product, device/motorola/sm6225-common/bengal.mk)
 
+# Audio - Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
