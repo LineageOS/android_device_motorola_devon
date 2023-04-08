237416b08619db41e670f031444735fabacef615
Dhina17 <dhinalogu@gmail.com>
2023-04-08 16:07:49 +0530

    hawao: Import odm sku manifests
    Change-Id: Ia41bd580fc8dc698033122751716485eb2001fe0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,17 @@ TARGET_BOOTLOADER_BOARD_NAME := hawao
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# HIDL
+ODM_MANIFEST_SKUS += b bc d dc dn dnc n nc
+ODM_MANIFEST_B_FILES := $(DEVICE_PATH)/sku/manifest_b.xml
+ODM_MANIFEST_BC_FILES := $(DEVICE_PATH)/sku/manifest_bc.xml
+ODM_MANIFEST_D_FILES := $(DEVICE_PATH)/sku/manifest_d.xml
+ODM_MANIFEST_DC_FILES := $(DEVICE_PATH)/sku/manifest_dc.xml
+ODM_MANIFEST_DN_FILES := $(DEVICE_PATH)/sku/manifest_dn.xml
+ODM_MANIFEST_DNC_FILES := $(DEVICE_PATH)/sku/manifest_dnc.xml
+ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/sku/manifest_n.xml
+ODM_MANIFEST_NC_FILES := $(DEVICE_PATH)/sku/manifest_nc.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/ext_config/hawao-default.config
 
