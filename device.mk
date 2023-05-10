d06debae044fca05ec90e6ce4963ab0c31818031
Michael Bestas <mkbestas@lineageos.org>
2023-05-10 22:45:17 +0300

    devon: Decommonize NFC permissions
    Change-Id: I9276c527d0899e29866419aad35e02b8cfaec5c7

--- device.mk
+++ device.mk
@@ -35,6 +35,12 @@ PRODUCT_PACKAGES += \
     init.mmi.overlay.rc
 
 # Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
+    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
+
 DEVICE_SKUS := b d f
 
 PRODUCT_COPY_FILES += \
