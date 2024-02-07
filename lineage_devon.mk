7aab8d28d5e551f49e5d901452de222b63c8358c
Michael Bestas <mkbestas@lineageos.org>
2024-02-08 01:39:15 +0200

    devon: Update to T2SNS33.73-22-3-3
    Change-Id: I8d1bf9efa156a2d71d6d0183c1d2f3ece6884273

--- lineage_devon.mk
+++ lineage_devon.mk
@@ -22,8 +22,8 @@ PRODUCT_MODEL := moto g32
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-BUILD_FINGERPRINT := "motorola/devon_g/devon:13/T2SNS33.73-22-3-2/61221-f86875:user/release-keys"
+BUILD_FINGERPRINT := "motorola/devon_g/devon:13/T2SNS33.73-22-3-3/5e905-fe2ab:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=devon_g \
-    PRIVATE_BUILD_DESC="devon_g-user 13 T2SNS33.73-22-3-2 61221-f86875 release-keys"
+    PRIVATE_BUILD_DESC="devon_g-user 13 T2SNS33.73-22-3-3 5e905-fe2ab release-keys"
