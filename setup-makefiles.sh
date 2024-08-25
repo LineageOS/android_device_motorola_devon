f50bdd8d1090b6b3195c91fd47925a2334c92b92
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 11:26:19 +0300

    devon: Override lib_to_package_fixup
    Change-Id: Ief81b15828be19d37be56eb98d1e36106e6dbc72

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -11,6 +11,11 @@ function vendor_imports() {
 EOF
 }
 
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1"
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
