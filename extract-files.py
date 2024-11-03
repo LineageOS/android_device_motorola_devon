b21233d2119f809167bbb1b07ac22214791a1e89
Michael Bestas <mkbestas@lineageos.org>
2024-11-03 07:33:51 +0200

    devon: Switch to python extract-utils
    Change-Id: Ida4b83459d9739ac880b26a91df974e1db8423e9

---
+++ extract-files.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.extract import extract_fns_user_type
+from extract_utils.extract_star import extract_star_firmware
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "vendor/motorola/sm6225-common",
+    "vendor/qcom/opensource/display",
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/init/android.hardware.nfc@1.2-service.st-moto.rc': blob_fixup()
+        .regex_replace('class hal', 'override\n    class hal'),
+    ('vendor/lib64/camera/components/com.qti.node.gpu.so', 'vendor/lib64/hw/camera.qcom.so'): blob_fixup()
+        .binary_regex_replace(b'camera.mot.is.coming.cts', b'vendor.camera.coming.cts'),
+}  # fmt: skip
+
+extract_fns: extract_fns_user_type = {
+    r'(bootloader|radio)\.img': extract_star_firmware,
+}
+
+module = ExtractUtilsModule(
+    'devon',
+    'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    extract_fns=extract_fns,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+    add_generated_carriersettings=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(module, 'sm6225-common', module.vendor)
+    utils.run()
