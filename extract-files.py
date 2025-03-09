8b34ae723d50e432c543930cfc65f7423ddf950b
Michael Bestas <mkbestas@lineageos.org>
2025-03-09 12:45:54 +0200

    devon: Shim STFlashTool with libbase_shim
    See: https://cs.android.com/android/_/android/platform/system/libbase/+/46fc805282265715266c256fbe34cf1a886c475f
    
    Change-Id: I5127020e18114a13515d9dc382c95d689634243d

--- extract-files.py
+++ extract-files.py
@@ -29,6 +29,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/bin/STFlashTool': blob_fixup()
+        .add_needed('libbase_shim.so'),
     'vendor/etc/init/android.hardware.nfc@1.2-service.st-moto.rc': blob_fixup()
         .regex_replace('class hal', 'override\n    class hal'),
     ('vendor/lib64/camera/components/com.qti.node.gpu.so', 'vendor/lib64/hw/camera.qcom.so'): blob_fixup()
