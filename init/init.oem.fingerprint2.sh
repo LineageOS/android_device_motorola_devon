bec9c266d2ce1ee93fd3cfeb53d9b0501013d6a9
Michael Bestas <mkbestas@lineageos.org>
2025-05-08 05:23:01 +0300

    devon: Update init.oem.fingerprint2.sh based on fogo
    Change-Id: I0f5de93b8130a8d0677fed37ff46680741558f0c

--- init/init.oem.fingerprint2.sh
+++ init/init.oem.fingerprint2.sh
@@ -27,12 +27,11 @@ GKI_PATH=$(getprop $PROP_GKI_PATH)
 # hal_list: the array contains the hal service name.
 #
 # note: all arrays should have the same size.
-
 vendor_list=('fpc' 'egis')
 kernel_so_list=("/vendor/lib/modules/$GKI_PATH/fpc1020_mmi.ko" "/vendor/lib/modules/$GKI_PATH/ets_bix_mmi.ko")
 kernel_so_name_list=("fpc1020_mmi.ko" "ets_bix_mmi.ko")
+dev_node_list=("/sys/class/fingerprint/fpc1020/irq" "/dev/esfp0")
 hal_list=('fps_hal' 'ets_hal')
-
 last_vendor_index=`expr ${#vendor_list[@]} - 1`
 vendor_list_size=${#vendor_list[@]}
 
@@ -58,7 +57,7 @@ persist_fps_id=/mnt/vendor/persist/fps/vendor_id
 persist_fps_id2=/mnt/vendor/persist/fps/last_vendor_id
 
 FPS_VENDOR_NONE=none
-MAX_TIMES=20
+MAX_TIMES=50
 
 # this property store FPS_STATUS_NONE or FPS_STATUS_OK
 # after start fingerprint hal service, the hal service will set this property.
@@ -92,8 +91,22 @@ function start_hal_service(){
     setprop $prop_fps_ident $FPS_STATUS_NONE
 
     insmod ${kernel_so_list[$1]}
+    log "${kernel_so_list[$1]} insmod"
     sleep 1
     setprop $prop_fps_ident ${vendor_list[$1]}
+
+    retry=1
+    while [ $retry -lt 5 ]
+    do
+        if [ -e  "${dev_node_list[$1]}" ]; then
+            log "dev node : ${dev_node_list[$1]} is ready"
+            break
+        else
+            log "driver is not ready, $retry times wait 1s to retry"
+            sleep 1
+        fi
+        let retry++
+    done
 
     log "start ${hal_list[$1]}"
     start ${hal_list[$1]}
